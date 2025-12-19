150c0100f26db0462209d29bd85fe87905f0baa7
abhishek <abhisheksathala296@gmail.com>
2025-12-19 17:18:38 +0530

    aadd

--- src/components/redux/rootReducer.jsx
+++ src/components/redux/rootReducer.jsx
@@ -1,1 +1,8 @@
 import { combineReducers } from 'redux';
+import { PizzaReducer } from './pizza/PizzaReducers';
+import { BurggerReducer } from './burgger/BruggerReducers';
+
+export const rootReducer = combineReducers({
+  pizza: PizzaReducer,
+  burger: BurggerReducer,
+});
