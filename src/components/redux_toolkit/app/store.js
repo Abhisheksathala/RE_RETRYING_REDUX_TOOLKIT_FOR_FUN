782b10ad1552d98bc80176162d4512f018aad405
abhishek <abhisheksathala296@gmail.com>
2025-12-30 14:08:16 +0530

    add

--- src/components/redux_toolkit/app/store.js
+++ src/components/redux_toolkit/app/store.js
@@ -1,9 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
 import pizzaReducer from '../features/pizza/pizzaSlice.js';
+import reduxLogger from 'redux-logger';
+
+const logger = reduxLogger.createLogger({
+  collapsed: true, // Collapse logs by default
+  diff: true, // Show diff between states
+});
 
 const store = configureStore({
   reducer: {
     pizza: pizzaReducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(logger);
+    // OR using spread operator (alternative syntax)
+    // return [...getDefaultMiddleware(), logger];
   },
 });
 
--- src/components/redux_toolkit/index.js
+++ src/components/redux_toolkit/index.js
@@ -2,7 +2,7 @@ import store from './app/store.js';
 import { pizzaActions } from './features/pizza/pizzaSlice.js';
 console.log('initial state ', store.getState());
 const unsubscribe = store.subscribe(() => {
-  console.log('updated staet', store.getState());
+  // console.log('updated staet', store.getState());
 });
 store.dispatch(pizzaActions.pizza_order());
 unsubscribe();
